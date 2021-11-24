d608a143d77778533efabc2543e94d7efeec4aea
Discordmodsbers <94165954+Discordmodsbers@users.noreply.github.com>
2021-11-24 13:28:25 -0500

    dronev5

---
+++ Dronev5.py
@@ -0,0 +1,91 @@
+import random
+import socket
+import struct
+import sys
+import os
+print("Place Device Under Drone-")
+import time
+time.sleep(3)
+print("-Injecting Maliware")
+print("Closing injector file")
+print("DONE!")
+time.sleep(10)
+os.system ('clear')
+print("paid key")
+k = input("")
+if k == '1980':
+    os.system ('clear')
+    print("thank you")
+
+else:
+	os.system('clear')
+	sys.exit()
+time.sleep(10)
+print("listing ip drones in area")
+for x in range (1, 21):
+	print(y)
+print("drones in area")
+time.sleep(1)
+os.system ('clear')
+print("57.18.59.122 1")
+import time
+time.sleep(1)
+print("104.233.202.42")
+import time
+time.sleep(1)
+print("108.120.226.108")
+import time
+time.sleep(1)
+print("118.242.98.157")
+import time
+time.sleep(2)
+print("120.244.135.190")
+v = input("your drone ip")
+if v == '1':  
+ import time
+time.sleep(1)
+os.system ('clear')
+
+print("Fly:")
+print("Crash")
+print("Infect other drones")
+print("RemotCTRL")
+print("list random things")
+print("Permantly Inject")
+print("Next Page 'P'")
+x = input("Enter number : ")
+if  x == '1':
+   os.system ('clear')
+   input("How High you want it")
+   print("loading up")
+elif x == '2':
+   os.system ('clear')
+   print("GET REKT")
+elif x == '3':
+	os.system ('clear')
+	print("Takes a while-")
+	print("-Very useful to get near drone")
+	import time
+	time.sleep(5)
+	print("Done")
+	print("the infected drone only mirrors your drone")
+elif x == '4':
+	os.system ('clear')
+	print("1: Follow")
+elif x == '5':
+	os.system ('clear')
+	print("Injecting Backdoor")
+	time.sleep(3)
+	print("LzRat.v7")
+	time.sleep(3)
+	print("Installing LolCat")
+	time.sleep(3)
+	print("restart script please")
+elif x == 'p' or 'P':
+	os.system ('clear')
+	print("WELCOME TO THE MOD MENU PAGE")
+	print("Listing Mod Menus")
+	time.sleep(3)
+	print("Apex Legends")
+	print("Roblox")
+h = input("")
